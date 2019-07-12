f6700892c01fd5583d6de93dbe4ab80c8d834270
jigyasa07 <51105890+jigyasa07@users.noreply.github.com>
2019-07-12 14:24:52 +0530

    Update client1.rs

--- src/client1.rs
+++ src/client1.rs
@@ -2,28 +2,44 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::thread;
+use std::collections::HashMap;
+use std::clone::Clone;
 
 
-static NTHREADS: i32 = 750;
+//static NTHREADS: i32 = 7;
+
+fn handle_client(c:&mut HashMap<i32, TcpStream>,count:&i32){
+    loop{
+    let mut buf = [0u8; 512];
+    c.get_mut(&count).unwrap().write_all(buf.as_ref()).unwrap();
+    println!("data sent {}",count);
+    }
+}
+
 
 fn main() {
-    let mut vec = Vec::new();
+    let mut c = HashMap::new();
 
-    for i in 0..NTHREADS {
+    for i in 0..2 {
         let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
-        vec.push(stream);
+        c.insert(i,stream);
     }
-    for i in 0..NTHREADS
+    for i in 0..2
     {
-    thread::spawn(|| {
-        loop {
-                let mut buf = [0u8; 512];
+        let count =i;
+        //handle_client(&mut c,& count);
+        //let x: HashMap< i32 ,TcpStream> = c.iter().clone();//.collect();
+        //x.clone_from(&c);
+        thread::spawn(move|| handle_client(&mut c,& count));
+        // thread::spawn(|| {
+        // loop {
+        //         let mut buf = [0u8; 512];
+        //         let mut count = i;
+        //         //println!("thread {}: Sending over ", i);
+        //         c.get_mut(&count).unwrap().write_all(buf.as_ref()).unwrap();
 
-                //println!("thread {}: Sending over ", i);
-                vec[i].write_all(buf.as_ref()).unwrap();
-
-        }    
-        });
+        // }    
+        // });
     }
    
 }
